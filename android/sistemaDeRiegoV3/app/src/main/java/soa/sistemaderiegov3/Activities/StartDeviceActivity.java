98db07deb370a74b9b797959ea930439b0abe3f4
FGPena <facundogarciapena@hotmail.com>
2018-06-20 21:36:45 -0300

    adding bluetoothService

--- android/sistemaDeRiegoV3/app/src/main/java/soa/sistemaderiegov3/Activities/StartDeviceActivity.java
+++ android/sistemaDeRiegoV3/app/src/main/java/soa/sistemaderiegov3/Activities/StartDeviceActivity.java
@@ -1,11 +1,14 @@
 package soa.sistemaderiegov3.Activities;
 
 import soa.sistemaderiegov3.R;
+import soa.sistemaderiegov3.Services.BluetoothService;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class StartDeviceActivity extends AppCompatActivity {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
